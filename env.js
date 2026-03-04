e259ceef17288a1b3720a7654476ea49cd8b808e
samimosmansabuj <samim.o.sabuj01@gmail.com>
2026-03-04 12:14:27 +0600

    eng set

--- env.js
+++
@@ -1,6 +0,0 @@
-window.ENV = {
-  // API_BASE_URL: "http://127.0.0.1:8000"
-  API_BASE_URL: "https://apitest.pencilwoodbd.org",
-  WHATSAPP_NUMBER: "8801810447799",
-  FACEBOOK_DOMAIN_VERIFICATION: "oz5eq1vxre5dkpwyoc46r4uthzw46m"
-};
