f1a72abc668bc75216dc12022eb047dea66cd86d
samimosmansabuj <samim.o.sabuj01@gmail.com>
2026-03-04 12:36:47 +0600

    new update fb domain verify

--- js/main.js
+++ js/main.js
@@ -1,23 +1,23 @@
-if (window.ENV && window.ENV.FACEBOOK_DOMAIN_VERIFICATION) {
-    const fbMetaTag = document.querySelector(
-        'meta[name="facebook-domain-verification"]'
-    );
-    console.log("fbMetaTag: ", fbMetaTag);
-    if (fbMetaTag) {
-        fbMetaTag.setAttribute(
-            'content',
-            window.ENV.FACEBOOK_DOMAIN_VERIFICATION
-        );
-    } else {
-        const meta = document.createElement('meta');
-        meta.setAttribute('name', 'facebook-domain-verification');
-        meta.setAttribute(
-            'content',
-            window.ENV.FACEBOOK_DOMAIN_VERIFICATION
-        );
-        document.head.appendChild(meta);
-    }
-}
+// if (window.ENV && window.ENV.FACEBOOK_DOMAIN_VERIFICATION) {
+//     const fbMetaTag = document.querySelector(
+//         'meta[name="facebook-domain-verification"]'
+//     );
+//     console.log("fbMetaTag: ", fbMetaTag);
+//     if (fbMetaTag) {
+//         fbMetaTag.setAttribute(
+//             'content',
+//             window.ENV.FACEBOOK_DOMAIN_VERIFICATION
+//         );
+//     } else {
+//         const meta = document.createElement('meta');
+//         meta.setAttribute('name', 'facebook-domain-verification');
+//         meta.setAttribute(
+//             'content',
+//             window.ENV.FACEBOOK_DOMAIN_VERIFICATION
+//         );
+//         document.head.appendChild(meta);
+//     }
+// }
 
 document.addEventListener('DOMContentLoaded', () => {
     // ================= DATA =================
